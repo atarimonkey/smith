4f5400d83febcb7a263be3bf9580603718a6b4a8
atarimonkey <david.keuchel@gmail.com>
2016-09-25 16:22:51 -0500

    Add files via upload

--- baseline.py
+++ baseline.py
@@ -23,15 +23,106 @@
 # This will run the first run of the equipment to handle setting a baseline
 # for all future comparisons
 #
+import json
+import Tstat
+import senscom
+import time
 
 baseline = {}
+boards = {}
+data = {}
+
+with open('settings.json') as f:
+    base = json.load(f)
 
 # test steady state
+def testBoards():
+    h = senscom.hbotPing()
+    c = senscom.cbotPing()
+    boards.update({'heatbot': h, 'coolbot': c})
 # read all sensors
+def testSensors():
+    if not Tstat.tstatRead():
+        sp = senscom.supplyPress
+        rp = senscom.returnPress
+        dp = sp - rp
+        if (-3.0) <= dp <= (3.0):
+            pressAdj = dp
+        else:
+            pressAdj = dp
+            # warning high press adj check sensor
+        if senscom.hbotPing():
+            i = senscom.inducer()
+            if i > 0.1:
+                # warning ind amps sensed
+                pass
+            f = senscom.flame()
+            if f == True:
+                # warning flame sensed no call
+                pass
+            b = senscom.blower()
+            if b > 0.1:
+                # warning blower amps sensed
+                pass
+        if senscom.cbotPing():
+            odt = senscom.odt()
+            if not odt:
+                # warning odt sensor failure
+                pass
+            cf = senscom.cond_fan
+            if cf > 0.1:
+                # warning cond fan amps sensed
+                pass
+            comp = senscom.compressor
+            if comp > 0.1:
+                # warning compressor amps sensed
+                pass
+    while Tstat.tstatRead() != 'g':
+        time.sleep(1)
+    st = senscom.supplyTemp()
+    rt = senscom.returnTemp()
+    if (-3.0) <= (st - rt) <= (3.0):
+        tempAdj = st-rt
+    else:
+        tempAdj = st-rt
+        # warning high temp adj check sensors/location
+    sh = senscom.supplyHumid
+    rh = senscom.returnHumid
+    if (-3.0) <= (sh - rh) <= (3.0):
+        humAdj = sh - rh
+    else:
+        humAdj = sh - rh
+        # warning high humid adj check sensor/location
 # heat bot
+def ind(x):
     # inducer
+    stage = x
+    i = senscom.inducer()
+    if stage = 'low':
+        baseline.update({'inducer low': i})
+    else:
+        baseline.update({'inducer high': i})
+def fla():
     # flame sensor
+    fl = senscom.flame()
+    return fl
+def blo(c, x):
     # blower
+    call = c
+    stage = x
+    bl = senscom.blower()
+    if call == 'g':
+        baseline.update({'blower fan': bl})
+    elif call == 'y' and stage == 'low':
+        baseline.update({'blower cool low': bl})
+    elif call == 'y' and stage == 'high':
+        baseline.update({'blower cool high': bl})
+    elif call == 'w' and stage == 'low':
+        baseline.update({'blower heat low': bl})
+    elif call == 'w' and stage == 'high':
+        baseline.update({'blower heat high': bl})
+    else:
+        pass
 # main bot
     # return temp
     # supply temp
